ca5460caa31ab519207b01469eba6ec7f3f892c7
Felix Paradis <felixparadis@gmail.com>
2020-12-07 20:17:15 -0500

    wget: download whole website

---
+++ wget.bash
@@ -0,0 +1,3 @@
+# Download whole website
+# based on https://www.linuxjournal.com/content/downloading-entire-web-site-wget
+wget -E -H -k -K -p --recursive --domains paradis2019.org http://www.paradis2019.org
