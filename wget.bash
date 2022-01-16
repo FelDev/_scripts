a970c022339b8ed06aef6b4cb3cedb1d449ff46f
Felix Paradis <allo@felixparadis.com>
2022-01-15 23:12:07 -0600

    add .gitignore

--- wget.bash
+++ wget.bash
@@ -2,4 +2,14 @@
 # Create and cd into a new folder than run the command
 # based on https://www.linuxjournal.com/content/downloading-entire-web-site-wget
 mkdir old && cd old
-wget -E -H -k -K -p --recursive --domains paradis2019.org http://www.paradis2019.org
+
+wget -E -H -k -K -p --recursive --domains noltonlake.com http://www.noltonlake.com
+
+# # # # # # # # # # # # # # # # # # 
+# Basic Bash
+# # # # # # # # # # # # # # # # # # 
+# print all files recursively with particular extension
+find . -name "*.orig" -type f
+
+# add -delete command at the end to delete them
+find . -name "*.orig" -type f -delete
